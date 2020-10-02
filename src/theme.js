5ddf76ca9e086014277d24f17cc6920d42d4862a
peter <peter@peters-MacBook-Pro.local>
2020-10-02 15:25:36 -0700

    more color fixes

--- src/theme.js
+++ src/theme.js
@@ -7,7 +7,7 @@ export const backgroundColor = theme('mode', {
 
 export const textColor = theme('mode', {
     light: 'rgb(192, 192, 192)',
-    dark: 'rgb(90, 90, 90)'
+    dark: 'rgba(195, 195, 195, 0.555)'
 });
 
 export const accentColor = theme('mode', {
@@ -16,6 +16,6 @@ export const accentColor = theme('mode', {
 });
 
 export const hoverColor = theme('mode', {
-    light: 'rgba(255, 255, 255, 1)',
-    dark: 'rgba(150, 150, 150, 1)'
+    light: 'rgba(150, 150, 150, 1)',
+    dark: 'rgba(195, 195, 195, 0.8)'
 });

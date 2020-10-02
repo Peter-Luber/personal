b61c5617ef6481a32198e6f98087b3aec43ae204
peter <peter@peters-MacBook-Pro.local>
2020-10-02 16:02:53 -0700

    added gewzer site to portfolio

--- src/components/display/Portfolio.js
+++ src/components/display/Portfolio.js
@@ -4,10 +4,11 @@ class Portfolio extends React.Component {
   render() {
     return (
       <div id="Portfolio" className="page">
-        {/* <iframe title="gewzer" width="100%" height="100%" frameBorder="1" src="http://157.245.190.160/"></iframe> */}
-        <a href="http://tylerbenz.net" target="_blank" rel="noopener noreferrer">Tyler Benz portfolio</a>
+        <a href="http://tylerbenz.net" target="_blank" rel="noopener noreferrer">tyler benz portfolio</a>
         <br></br>
         <a href="http://157.245.190.160/" target="_blank" rel="noopener noreferrer">javascript/HTML5 music player</a>
+        <br></br>
+        <a href="https://www.gewzer.online">gewzer landing page</a>
       </div>
     );
   }
